5efadd29164699dde404bebbcf4687e0c3405bc7
Marcos Faneli <marcosfaneli@gmail.com>
2022-09-26 15:56:15 -0300

    create usecase user estimative

---
+++ src/api/usecase/new-user-estimate-use-case.ts
@@ -0,0 +1,18 @@
+import { RoomRepository } from "../infra/room-repository"
+
+export class NewUserEstimateUseCase {
+  constructor(private roomRepository: RoomRepository)
+  execute(request: any) {
+    const room = this.roomRepository.getRoomById(request.room)
+
+    if (!room) throw new Error("Invalid room")
+
+    const member = room.getMemberById(request.member)
+
+    if (!member) throw new Error("Invalid member")
+
+    member.estimate(room.activeRound, request.value)
+
+    this.roomRepository.save(room)
+  }
+}
