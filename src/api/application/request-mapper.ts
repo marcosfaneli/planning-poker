994d4aaa20504a00c79b5b56a1d2d7e0788e1ed8
Marcos Faneli <marcosfaneli@gmail.com>
2022-09-26 16:12:07 -0300

    create use case new room

--- src/api/application/request-mapper.ts
+++ src/api/application/request-mapper.ts
@@ -1,5 +1,16 @@
+import { Room } from "../domain/room"
+import { Member } from "../domain/member"
+
 export class RequestMapper {
   static toMember(request: any): Member {
     return new Member(request.name)
+  }
+
+  static extractOwnerRoom(request: any): Member {
+    return new Member(request.userName)
+  }
+
+  static toRoom(request: any, owner: Member): Room {
+    return new Room(owner, request.roomName);
   }
 }
--- src/api/usecase/new-room-use-case.ts
+++ src/api/usecase/new-room-use-case.ts
@@ -1,1 +1,12 @@
-export class New
+import { RequestMapper } from "../application/request-mapper"
+import { RoomRepository } from "../infra/room-repository"
+
+export class NewRoomUseCase {
+  constructor(private roomRepository: RoomRepository)
+  execute(request: any) {
+    const owner = RequestMapper.extractOwnerRoom(request)
+    const room = RequestMapper.toRoom(request, owner)
+
+    this.roomRepository.save(room)
+  }
+}
